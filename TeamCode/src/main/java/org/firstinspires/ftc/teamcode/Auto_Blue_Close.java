9a504cf0e58c01c3462e593a50c68c6038b67748
Gabriela Lopez <gabrielalopez111302@gmail.com>
2018-11-01 15:57:35 -0400

    Auto_Blue_Close

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_Close.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_Close.java
@@ -95,13 +95,15 @@ public class Auto_Blue_Close extends LinearOpMode {
             //Robot stops moving for first 15 seconds of Autonomous Mode. Do if other robot going first
         robot.Forward_for_Distance(55, .5);
             //robot moves forward for 48 at 50% speed
+        robot.StopMoving(2, runtime);
+            //StopMoving for 2 seconds
         robot.Backward_for_Distance(8, .5);
         robot.Left_for_Distance(90, .5);
             //robot turns left, 360 degrees at 50% speed
         robot.DumpIt(runtime);
             //Dumps the symbol thing
         robot.Left_for_Distance(185, .5);
-            //robot turns left, 98 degrees at 50% speed
+            //robot turns left, 185 degrees at 50% speed
         robot.Forward_for_Distance(95, 1);
             //robot moves forward for 90 at 100% speed)
         robot.StopMoving(1.0, runtime);
